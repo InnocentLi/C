64c25fb14d72fed475cf4f29bb96e47134215365
innocence <vincentlieie@gmail.com>
2022-06-26 09:02:04 +0900

    関数ポインタ

--- 029dynamic_memory.c
+++ 029dynamic_memory.c
@@ -4,7 +4,40 @@
 
 #include "stdlib.h"
 #include "stdio.h"
+#include "io_utils.h"
+
+#define PLAYER_COUNT 10
+
+void InitPointer(int **ptr,int length,int default_value){
+    *ptr = malloc(sizeof (int)*length);
+    for(int i=0;i<length;i++){
+        (*ptr)[i] = default_value;
+    }
+
+}
 
 int main(){
+    int *players;
+    InitPointer(&players,10,0);
 
+   // int * players = malloc(sizeof (int) * PLAYER_COUNT);
+    for(int i = 0;i<PLAYER_COUNT;++i){
+        players[i] = i;
+    }
+
+
+    PRINT_INT_ARRAY(players,PLAYER_COUNT);
+    // 不加free可能也会被回收
+
+    //players = realloc()
+    //
+//    常见指针使用错误
+//    忘记释放内存
+//    使用了已经释放的内存
+//    是用了超出边界的内存
+//    改变内存指针导致无法正常释放
+
+    free(players);
+
+    return 0;
 }
---
+++ 030function_pointer.c
@@ -0,0 +1,10 @@
+//
+// Created by Innocent on 6/26/22.
+// 函数指针関数ポインタ
+#include "stdlib.h"
+#include "stdio.h"
+#include "io_utils.h"
+
+int main(){
+
+}
